33310f4ebfad222c5358261635098cd6cb5ee7c0
Castellano46 <pedroliebana35@gmail.com>
2024-09-22 09:31:48 +0200

    On-Boarding

--- app/src/main/java/com/example/tu_primer_lambo/ui/view/fragments/OnboardingFragment.kt
+++ app/src/main/java/com/example/tu_primer_lambo/ui/view/fragments/OnboardingFragment.kt
@@ -5,20 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tu_primer_lambo.R
 import com.example.tu_primer_lambo.databinding.FragmentOnboardingBinding
 import com.example.tu_primer_lambo.ui.view.activities.MainActivity
 
 class OnboardingFragment : Fragment() {
 
     private lateinit var binding: FragmentOnboardingBinding
-    private val story = listOf(
-        "Era un día como otro cualquiera, te despiertas con resaca y algo en ti ha cambiado.",
-        "Decides que es hora de tomar el control de tu vida. No mas PANZA, no mas conformismo",
-        "No más excusas...se acabó ser un fakin mileurista",
-        "Sabías que para lograr tu sueño y tener tu primer Lambo, tenías que ser disciplinado.",
-        "La clave la tiene el gran Llados, tengo que hacer caso de él y de nadie más.",
-        "Cada día cuenta, cada burpee te acerca más a tu meta.",
-        "Fakin inutil ¿Estás listo para empezar este reto y ver cómo te transformas masivamente?"
+    private val storyImages = listOf(
+        R.drawable.onboarding_1,
+        R.drawable.onboarding_2,
+        R.drawable.onboarding_3,
+        R.drawable.onboarding_4,
+        R.drawable.onboarding_5,
+        R.drawable.onboarding_6,
+        R.drawable.onboarding_7
     )
     private var currentStoryIndex = 0
 
@@ -35,7 +36,7 @@ class OnboardingFragment : Fragment() {
         updateStory()
 
         binding.btnNext.setOnClickListener {
-            if (currentStoryIndex < story.size - 1) {
+            if (currentStoryIndex < storyImages.size - 1) {
                 currentStoryIndex++
                 updateStory()
             } else {
@@ -45,6 +46,6 @@ class OnboardingFragment : Fragment() {
     }
 
     private fun updateStory() {
-        binding.txtStory.text = story[currentStoryIndex]
+        binding.imgStory.setImageResource(storyImages[currentStoryIndex])
     }
 }
